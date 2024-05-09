f035c32803dd09191734ad6407f19bfff266b85b
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2024-05-10 01:44:38 +0200

    Profile Page terminated

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
-
+use Doctrine\ORM\EntityManagerInterface; // Import EntityManagerInterface
+use App\Entity\Campaigns; // Import the Campaign entity
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'Profile_Page')]
-    public function show_profile(Security $security): Response
-    {   $user = $security->getUser();
+    public function show_profile(Security $security, ManagerRegistry $entityManager): Response
+    {
+        $user = $security->getUser();
+
+        // Fetch campaigns associated with the logged-in user
+        $campaigns = $entityManager->getRepository(Campaigns::class)->findBy(['OwnerId' => $user]);
+
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
-            'fullname'=> $user->getFullname()
+            'fullname' => $user->getFullname(),
+            'campaigns' => $campaigns, // Pass campaigns to the template
         ]);
     }
 }
