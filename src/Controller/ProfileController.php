d83aeab8470b5f5c40302dd5ef973c2ebc75689a
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2024-05-10 03:09:08 +0200

    delete Campaign is possible

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -26,4 +26,23 @@ class ProfileController extends AbstractController
             'campaigns' => $campaigns, // Pass campaigns to the template
         ]);
     }
+    #[Route('/campaign/delete/{id}', name: 'delete_campaign')]
+    public function deleteCampaign(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $campaign = $entityManager->getRepository(Campaigns::class)->find($id);
+
+        if (!$campaign) {
+            throw $this->createNotFoundException('Campaign not found');
+        }
+
+        // Check if the logged-in user is the owner of the campaign
+        if ($campaign->getOwnerId() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('You are not allowed to delete this campaign');
+        }
+
+        $entityManager->remove($campaign);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('Profile_Page');
+    }
 }
